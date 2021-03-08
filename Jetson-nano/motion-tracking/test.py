817fc0a96abbe82b6647372a374e995ebe948876
lacie-life <00sao00ios00@gmail.com>
2021-03-08 15:54:20 +0700

    update motion r=tracking and failed code

---
+++ Jetson-nano/motion-tracking/test.py
@@ -0,0 +1,91 @@
+# import the necessary packages
+from imutils.video import VideoStream
+import argparse
+import datetime
+import imutils
+import time
+import cv2
+
+
+
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=1280,
+    display_height=720,
+    framerate=60,
+    flip_method=2,
+):
+    return (
+        "nvarguscamerasrc sensor_id = %d ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+def show_camera():
+    # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
+    print(gstreamer_pipeline(flip_method=2))
+    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=2), cv2.CAP_GSTREAMER)
+    firstFrame = None
+    if cap.isOpened():
+        window_handler = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
+
+        while cv2.getWindowProperty("CSI Camera", 0) >= 0:
+            ret_val, frame = cap.read()
+
+            frame = imutils.resize(frame, width = 500)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+            if firstFrame is None:
+                firstFrame = gray
+                continue
+                
+            frameDelta = cv2.absdiff(firstFrame, gray)
+            thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+            thresh = cv2.dilate(thresh, None, iterations=2)
+            cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cnts = imutils.grab_contours(cnts)
+
+            for c in cnts:
+                if cv2.contourArea(c) < 500:
+                    continue
+
+                (x, y, w, h) = cv2.boundingRect(c)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                text = "Occupied"
+                cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+                                    (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+                cv2.imshow("Security Feed", frame)
+                cv2.imshow("Thresh", thresh)
+                cv2.imshow("Frame Delta", frameDelta)
+
+            keyCode = cv2.waitKey(30) & 0xFF
+            if keyCode == 27:
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+    else:
+        print("Unable to open camera")
+
+if __name__ == "__main__":
+    show_camera()
