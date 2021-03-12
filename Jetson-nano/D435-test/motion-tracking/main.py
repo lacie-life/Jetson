d99b24fbd07b6f7a33c59f95b3d2ea63a82a1547
lacie-life <00sao00ios00@gmail.com>
2021-03-12 13:50:08 +0700

    update capture by python

--- Jetson-nano/D435-test/motion-tracking/main.py
+++ Jetson-nano/D435-test/motion-tracking/main.py
@@ -1,6 +1,11 @@
 import pyrealsense2 as rs
-import numpy as np
+from imutils.video import VideoStream
+import argparse
+import datetime
+import imutils
+import time
 import cv2
+
 
 # Configure depth and color streams
 pipeline = rs.pipeline()
@@ -21,33 +26,13 @@ pipeline.start(config)
 try:
     while True:
 
-        # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
         color_frame = frames.get_color_frame()
-        if not depth_frame or not color_frame:
+        if not color_frame:
             continue
-
-        # Convert images to numpy arrays
-        depth_image = np.asanyarray(depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
-
-        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-
-        depth_colormap_dim = depth_colormap.shape
-        color_colormap_dim = color_image.shape
-
-        # If depth and color resolutions are different, resize color image to match depth image for display
-        if depth_colormap_dim != color_colormap_dim:
-            resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
-            images = np.hstack((resized_color_image, depth_colormap))
-        else:
-            images = np.hstack((color_image, depth_colormap))
-
         # Show images
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', images)
+        cv2.imshow('RealSense', color_image)
         cv2.waitKey(1)
 
 finally:
