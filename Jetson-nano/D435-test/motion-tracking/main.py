5784e1b56dd9d8b5742257857bb37577c6cee668
lacie-life <00sao00ios00@gmail.com>
2021-03-12 14:25:11 +0700

    fixed color-image viewer

--- Jetson-nano/D435-test/motion-tracking/main.py
+++ Jetson-nano/D435-test/motion-tracking/main.py
@@ -1,11 +1,6 @@
 import pyrealsense2 as rs
-from imutils.video import VideoStream
-import argparse
-import datetime
-import imutils
-import time
+import numpy as np
 import cv2
-
 
 # Configure depth and color streams
 pipeline = rs.pipeline()
@@ -18,7 +13,11 @@ device = pipeline_profile.get_device()
 device_product_line = str(device.get_info(rs.camera_info.product_line))
 
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+if device_product_line == 'L500':
+    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+else:
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
 # Start streaming
 pipeline.start(config)
@@ -26,10 +25,14 @@ pipeline.start(config)
 try:
     while True:
 
+        # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
         color_frame = frames.get_color_frame()
         if not color_frame:
             continue
+
+        color_image = np.asanyarray(color_frame.get_data())
+
         # Show images
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('RealSense', color_image)
