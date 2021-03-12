8d5993784948f3bbc4be0c671f71a293dbf12e45
lacie-life <00sao00ios00@gmail.com>
2021-03-12 14:46:52 +0700

    motion tracking in D435

--- Jetson-nano/D435-test/motion-tracking/main.py
+++ Jetson-nano/D435-test/motion-tracking/main.py
@@ -1,5 +1,7 @@
 import pyrealsense2 as rs
 import numpy as np
+import datetime
+import imutils
 import cv2
 
 # Configure depth and color streams
@@ -23,8 +25,8 @@ else:
 pipeline.start(config)
 
 try:
+    firstFrame = None
     while True:
-
         # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
         color_frame = frames.get_color_frame()
@@ -32,11 +34,40 @@ try:
             continue
 
         color_image = np.asanyarray(color_frame.get_data())
+        fr = imutils.resize(color_image, width=500)
+        gray = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-        # Show images
-        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', color_image)
-        cv2.waitKey(1)
+        if firstFrame is None:
+            firstFrame = gray
+            continue
+
+        frameDelta = cv2.absdiff(firstFrame, gray)
+        thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+
+        for c in cnts:
+            if cv2.contourArea(c) < 1000:
+                continue
+
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(fr, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            text = "Occupied"
+            cv2.putText(fr, "Room Status: {}".format(text), (10, 20),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            cv2.putText(fr, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+                        (10, fr.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+        cv2.imshow("Security Feed", fr)
+        cv2.imshow("Thresh", thresh)
+        cv2.imshow("Frame Delta", frameDelta)
+
+        keyCode = cv2.waitKey(30) & 0xFF
+        if keyCode == 27:
+            break
+    cv2.destroyAllWindows()
 
 finally:
 
